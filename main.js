c95ad0f10782582c118225c849da8529f4226215
Trọng <quangtrong1506@gmail.com>
2023-05-28 02:48:06 +0700

    upload core

--- main.js
+++ main.js
@@ -1,81 +1,35 @@
-const { app, BrowserWindow, screen, globalShortcut, Tray, Menu, shell } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const { off } = require('process');
-let tray = null,
-    mainWindow = null;
+let mainWindow = null;
 
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 1300,
-        height: 900,
+        width: 700,
+        height: 400,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
-        minimizable: false,
         resizable: true,
-        frame: false,
-        x: 0,
-        y: 0,
     });
 
     // and load the index.html of the app.
     mainWindow.loadFile('index.html');
-    // cho nó xuống dưới
-    mainWindow.blur();
-    //xóa menu mặc định
     mainWindow.removeMenu();
-    // Max size
-    mainWindow.maximize();
-    // bỏ qua taskbar
-    mainWindow.setSkipTaskbar(true);
-
     mainWindow.webContents.openDevTools();
-
-    // Tray icon
-    tray = new Tray('./src/images/logo.ico');
-    let contextMenu = Menu.buildFromTemplate(trayMenu());
-    tray.setContextMenu(contextMenu);
-    tray.setToolTip('Màn hình nền tự build');
-
-    tray.on('click', () => {
-        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
-        mainWindow.blur();
-    });
 }
-
-function trayMenu() {
-    var text = mainWindow.isVisible() ? 'Ẩn hình nền' : 'Hiện hình nền';
-    let innerMenu = [
-        {
-            label: 'Dừng',
-            click: () => {
-                app.quit();
-            },
-        },
-        {
-            label: text,
-            click: () => {
-                mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
-                mainWindow.blur();
-
-                let contextMenu = Menu.buildFromTemplate(trayMenu());
-                tray.setContextMenu(contextMenu);
-            },
-        },
-        {
-            label: 'Liên hệ',
-            click: () => {
-                shell.openExternal('https://www.facebook.com/quangtrong.1506');
-            },
-        },
-    ];
-    return innerMenu;
+function LaunchExe() {
+    var child = require('child_process').execFile;
+    var executablePath = path.join(__dirname, 'Dragonboy_vn_v225.exe');
+    var parameters = [];
+    child(executablePath, parameters, function (err, data) {
+        console.log(err);
+        console.log(data.toString());
+    });
 }
 
 app.whenReady().then(() => {
     createWindow();
-
     app.on('activate', function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
